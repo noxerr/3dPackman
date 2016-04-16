de5d9692c6b2534400c95e94fda03837dcf7cd5f
Dani <dani_rl94@hotmail.com>
2016-04-16 20:51:22 +0200

    first commit ball walks on cube walls

---
+++ Pacman 3D/Assets/FollowPac.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowPac : MonoBehaviour {
+    public GameObject target;
+    /*public float xOffset = 0;
+    public float yOffset = 0;
+    public float zOffset = 0;
+    Vector3 offsets;*/
+    Vector3 relativePos;
+
+    void LateUpdate()
+    {
+        /*this.transform.position = new Vector3(target.transform.position.x + xOffset,
+                                              target.transform.position.y + yOffset,
+                                              target.transform.position.z + zOffset);*/
+        //transform.position = target.transform.position + offsets;
+        transform.position = target.transform.position + relativePos;
+        //Debug.Log(relativePos);
+        //Debug.Log(offsets);
+    }
+
+	// Use this for initialization
+	void Start () {
+        //offsets = new Vector3(xOffset, yOffset, zOffset);
+        relativePos = transform.position - target.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+}
---
+++ Pacman 3D/Assets/MovePlayer.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovePlayer : MonoBehaviour {
+    public GameObject platformCube;
+	// Use this for initialization
+    private Vector3 currentSpeed;
+    private Vector3 platformCenter;
+    private Vector3 radiusCenter;
+    private Rigidbody rb;
+
+	void Start () {
+        currentSpeed = new Vector3(0.0f, 0.0f, 0.0f);
+        platformCenter = platformCube.transform.position;
+        rb = GetComponent<Rigidbody>();
+	}
+
+    void Update()
+    {
+        radiusCenter = platformCenter - transform.position;
+        if (Mathf.Abs(radiusCenter.x) > Mathf.Abs(radiusCenter.y)){
+            if (Mathf.Abs(radiusCenter.x) > Mathf.Abs(radiusCenter.z))
+            {
+                //plano x
+                rb.AddForce(new Vector3(radiusCenter.x, 0, 0), ForceMode.Acceleration); //Physics.gravity
+                //transform.rotation.SetLookRotation(new Vector3(1, 0, 0), new Vector3(radiusCenter.x, 0, 0));
+                if (radiusCenter.x > 0) transform.rotation = Quaternion.Euler(0, 0, 90);
+                else transform.rotation = Quaternion.Euler(0, 0, -90);
+            }
+            else
+            {
+                rb.AddForce(new Vector3(0, 0, radiusCenter.z), ForceMode.Acceleration); //plano z
+                //transform.rotation.SetLookRotation(new Vector3(0, 0, 1), new Vector3(0, 0, radiusCenter.z));
+                transform.rotation = Quaternion.Euler(0, 0, 0);
+            }
+        }
+        else if (Mathf.Abs(radiusCenter.y) > Mathf.Abs(radiusCenter.z))
+        {
+            rb.AddForce(new Vector3(0, radiusCenter.y, 0), ForceMode.Acceleration); //Plano y
+            //transform.rotation.SetLookRotation(new Vector3(0, 1, 0), new Vector3(0, radiusCenter.y, 0));
+            transform.rotation = Quaternion.Euler(0, 0, 0);
+        }
+
+        else {
+            rb.AddForce(new Vector3(0, 0, radiusCenter.z), ForceMode.Acceleration); //plano z
+            //transform.rotation.SetLookRotation(new Vector3(0, 0, 1), new Vector3(0, 0, radiusCenter.z));
+            transform.rotation = Quaternion.Euler(0, 0, 0);
+        }
+
+
+        /*Renderer rend = platformCube.GetComponent<Renderer>();
+        Vector3 center = rend.bounds.center;*/
+    }
+
+	// Update is called once per frame
+	void FixedUpdate () {
+        
+        if (Input.GetKey(KeyCode.LeftArrow)){
+            currentSpeed.x = -(10f);
+        }
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            currentSpeed.x = 10f;
+        }
+        else currentSpeed.x = 0;
+
+        //move packman
+        this.transform.Translate(Time.fixedDeltaTime * currentSpeed.x, Time.fixedDeltaTime * currentSpeed.y,
+            Time.fixedDeltaTime * currentSpeed.z, Space.Self);
+	}
+}
