a9d8a5570e19b6ca5432f53d10c964165c2b7322
Outer2g <aol.treballs@gmail.com>
2016-05-03 11:18:39 +0200

    NavMesh

---
+++ Pacman 3D/Assets/Scripts/moveTo.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class moveTo : MonoBehaviour {
+    public Transform goal;
+    private NavMeshAgent agent;
+    // Use this for initialization
+	void Start () {
+        agent = GetComponent<NavMeshAgent>();
+        agent.SetDestination(goal.position);
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        agent.SetDestination(goal.position);
+
+    }
+}
