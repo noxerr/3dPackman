77505997959ad8a1ee81a5187b756d3cdaadca99
Dani <dani_rl94@hotmail.com>
2016-05-16 17:09:39 +0200

    pacman moves its leg

---
+++ Pacman 3D/Assets/Scripts/Animation.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Animation : MonoBehaviour {
+    private Transform[] childs;
+    private float elapsedTime;
+
+	// Use this for initialization
+	void Start () {
+        childs = GetComponentsInChildren<Transform>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        elapsedTime += Time.deltaTime;
+        childs[1].position = new Vector3(transform.position.x ,
+            transform.position.y, transform.position.z + Mathf.Cos(elapsedTime*3) * 3);
+        childs[5].position = new Vector3(transform.position.x,
+            transform.position.y, transform.position.z + Mathf.Cos(elapsedTime * 3) * 3);
+        childs[22].position = new Vector3(transform.position.x,
+            transform.position.y, transform.position.z + Mathf.Cos(elapsedTime * 3) * 3);
+        Debug.Log("Cosin: " + Mathf.Cos(elapsedTime));
+        Debug.Log("Time: " + elapsedTime);  
+        Debug.Log(childs[1].localPosition.x);
+        Debug.Log(childs[1].name);
+	}
+}
