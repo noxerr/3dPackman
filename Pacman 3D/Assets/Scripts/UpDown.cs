697bce18d81c5f055708b30a200cafcc8999063d
Dani <dani_rl94@hotmail.com>
2016-05-29 16:27:43 +0200

    added details and animations of lava on level 2

---
+++ Pacman 3D/Assets/Scripts/UpDown.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UpDown : MonoBehaviour {
+    Transform trans;
+    Vector3 Position;
+    public float div;
+    private float time;
+    private float y;
+	// Use this for initialization
+	void Start () {
+        trans = GetComponent<Transform>();
+        Position = trans.localPosition;
+        time = 0;
+        y = Position.y;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        time += Time.deltaTime*1.5f;
+        Position.y = y + Mathf.Sin(time)/div + 0.1f;
+        trans.localPosition = Position;
+	}
+}
