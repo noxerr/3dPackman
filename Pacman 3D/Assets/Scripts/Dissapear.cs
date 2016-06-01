ee9c2f0a20455a6bdd5d45a6ff9d64315214132d
Dani <dani_rl94@hotmail.com>
2016-06-01 19:46:09 +0200

    -Fixed lava look (overflows the terrain) -Switched wall by door that will open instead of dissapear -Added coin over the door so you can guess what to do -Added fences on the way to the cave so user won't try to go outside the path -Added alpha channel to trees near the cave

--- Pacman 3D/Assets/Scripts/Dissapear.cs
+++ Pacman 3D/Assets/Scripts/Dissapear.cs
@@ -5,16 +5,31 @@ public class Dissapear : MonoBehaviour {
     public GameObject pacman;
     public int MonedasAntesCueva;
     private PlayerLogic logica;
+    private bool animate, finishAnimate;
 
 	// Use this for initialization
 	void Start () {
         logica = pacman.GetComponent<PlayerLogic>();
+        animate = false;
+        finishAnimate = false;
 	}
 	
 	// Update is called once per frame
 	void FixedUpdate () {
-	    if (logica.monedasPilladas >= MonedasAntesCueva){
-            gameObject.SetActive(false);
+	    if (logica.monedasPilladas >= MonedasAntesCueva && !animate && !finishAnimate){
+            finishAnimate = true;
+            animate = true;
+            //gameObject.SetActive(false);
+            //enabled = false;
+        }
+        else if (animate && !finishAnimate)
+        {
+
+        }
+        else if (finishAnimate)
+        {
+            gameObject.GetComponent<BoxCollider>().enabled = false;
+            gameObject.SetActive(false); //hare que rote la puerta para simular que se abre
             enabled = false;
         }
 	}
